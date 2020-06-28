ed3cf01f9daf05ae202a8544143358f7073f6bf4
Angeline Lee <leeztangeline@gmail.com>
2020-06-28 14:48:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(cs) {
+document.getElementById("soon").style.display="none";
+document.getElementById("soon2").style.display="none";
+document.getElementById("soon3").style.display="none";
+document.getElementById("soon4").style.display="none";
+document.getElementById(cs).style.display="block";
+}
